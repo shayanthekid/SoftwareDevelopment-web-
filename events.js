0a23dfd5a0b36c6984bc802efb1139e343d70a76
unknown <IT@SAJID-LAP.apiit.ems>
2025-08-21 09:06:26 +0530

    second commit

--- events.js
+++ events.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 document.getElementById("myInput")
 .addEventListener('keyup', 
     function () {
@@ -51,3 +52,58 @@ document.getElementById("email")
     //or that it needs to be filled
 });
 
+=======
+document.getElementById("myInput")
+.addEventListener('keyup', 
+    function () {
+    document.getElementById("output")
+    .innerHTML = this.value;
+});
+
+
+
+
+//  this refers to the element that receives the event
+//In this case, its the input
+//Therefore, when the event is triggered by the input, the output paragraph's value
+//is changed to the input's value
+
+
+document.getElementById("box")
+.addEventListener('mouseover', function(){
+    this.style.background = "green";
+});
+
+document.getElementById("box")
+.addEventListener('mouseout', function(){
+    this.style.background = "blue";
+});
+
+// That makes it zoom in and out when hovered
+//and go back to normal when mouse out
+
+document.getElementById("text")
+.addEventListener("dblclick", function() {
+    this.style.color = "red";
+    this.style.backgroundColor = "blue";
+})
+
+//use the DB Click on a paragraph called Like
+// Show the heart emoji
+
+
+document.getElementById("email")
+.addEventListener("focus", function(){
+    this.style.border = "2px solid blue";
+    // Make the user know which input is 
+    //Priority 
+});
+
+document.getElementById("email")
+.addEventListener("blur", function(){
+    this.style.border = "4px solid gray";
+    //Can be used to say input is optional 
+    //or that it needs to be filled
+});
+
+>>>>>>> 18fddcf (second commit)
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 
 
@@ -19,3 +20,26 @@ document.documentElement.style.setProperty('--text-color', 'white');
 function setLightTheme() {
     
 }
+=======
+
+
+
+let h1 =document.getElementById("h1");
+
+
+function change(){
+    // h1.style.backgroundColor = "red";
+    h1.style.display = "none";
+  }    
+
+
+
+
+function setDarkTheme() {
+document.documentElement.style.setProperty('--bg-color', 'black');
+document.documentElement.style.setProperty('--text-color', 'white');
+}
+function setLightTheme() {
+    
+}
+>>>>>>> 18fddcf (second commit)
